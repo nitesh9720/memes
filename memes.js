b8b7d42b648d1d3ec0949d0c23453d72db920b36
NITESH KUMAR <tk972021@gmail.com>
2022-10-13 00:09:42 +0530

    first commit

--- memes.js
+++ memes.js
@@ -1,18 +1,22 @@
 const express = require("express");
 const app = express();
+app.set("view engine", "ejs");
 const https = require("https");
+
 app.get("/", function (req, res) {
+  // let imageUrl=""
   const url = "https://meme-api.herokuapp.com/gimme";
   https.get(url, function (response) {
     response.on("data", function (data) {
       const memes = JSON.parse(data);
-      var imageUrl = memes.url;
-      console.log(imageUrl);
-      res.send("<img src=" + imageUrl + ">");
+      let imageUrl = memes.url;
+      // console.log(imageUrl);
+      // res.send("<img src=" + imageUrl + ">");
+      return res.json({ url: imageUrl });
     });
   });
 });
 
-app.listen(process.env.PORT || 3000, function () {
+app.listen(3000, function () {
   console.log("server is running at port 3000");
 });
